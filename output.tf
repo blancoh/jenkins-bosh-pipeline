df3e9ab12624701af68880b3481fc48c16fec868
blancoh <blancoh@users.noreply.github.com>
2019-07-30 12:36:02 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,14 +1,3 @@
-#output "elb_dns_name" {
-#  value = "${aws_elb.elb.dns_name}"
-#}
-
-#output "alb_dns_name" {
-#  value = "${aws_lb.alb.dns_name}"
-#}
-
-#output "nlb_dns_name" {
-##  value = "${aws_lb.nlb.dns_name}"
-#}
 output "ec2_public_ip" {
   value = ["${aws_instance.jumpbox.*.public_ip}"]
 

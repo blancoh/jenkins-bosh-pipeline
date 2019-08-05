f27a62719410f73d936a12b606ad46aae0730974
blancoh <blancoh@users.noreply.github.com>
2019-08-04 20:20:39 -0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,3 @@
-# Create a VPC to launch our instances into
 resource "aws_vpc" "default" {
     cidr_block = "${var.vpc_cidr}"
     enable_dns_support = true
@@ -7,7 +6,6 @@ resource "aws_vpc" "default" {
       Name = "Bosh VPC"
     }
 }
-
 resource "aws_subnet" "PublicSubnetA" {
   vpc_id = "${aws_vpc.default.id}"
   cidr_block = "${var.public_subnet_a}"
@@ -26,7 +24,6 @@ resource "aws_subnet" "PrivateSubnetA" {
  #availability_zone = "${data.aws_availability_zones.available.names[0]}"
  availability_zone = "us-east-1a"
 }
-
 resource "aws_route_table_association" "PublicSubnetA" {
     subnet_id = "${aws_subnet.PublicSubnetA.id}"
     route_table_id = "${aws_route_table.public_route_a.id}"
@@ -35,7 +32,6 @@ resource "aws_route_table_association" "PrivateSubnetA" {
     subnet_id = "${aws_subnet.PrivateSubnetA.id}"
     route_table_id = "${aws_route_table.private_route_a.id}"
 }
-
 resource "aws_internet_gateway" "gw" {
    vpc_id = "${aws_vpc.default.id}"
     tags = {
@@ -50,7 +46,6 @@ resource "aws_nat_gateway" "public_nat_a" {
     subnet_id = "${aws_subnet.PublicSubnetA.id}"
     depends_on = ["aws_internet_gateway.gw"]
 }
-
 resource "aws_network_acl" "all" {
    vpc_id = "${aws_vpc.default.id}"
     egress {
@@ -73,7 +68,6 @@ resource "aws_network_acl" "all" {
         Name = "open acl"
     }
 }
-
 resource "aws_route_table" "public_route_a" {
   vpc_id = "${aws_vpc.default.id}"
   tags = {

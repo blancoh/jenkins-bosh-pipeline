3aa25cbd14efa201e3c6cead5f1f9d9b0d59a16c
abelanc0 <abelanc0@gmail.com>
2019-08-05 21:39:41 -0400

    Updated ec2_deploy_bosh.sh

--- ec2_deploy_bosh.sh
+++ ec2_deploy_bosh.sh
@@ -2,4 +2,6 @@
 
 ec2_publicip=`/usr/local/bin/aws ec2 describe-instances --filters "Name=tag:Name,Values=JumpBox" --query 'Reservations[*].Instances[*].PublicIpAddress' --output text`
 
+chmod 600 jumpbox.pem
+
 ssh -i jumpbox.pem ubuntu@$ec2_publicip "setup-bosh-env.sh"

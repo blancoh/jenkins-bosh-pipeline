630586e9ef8e609a028b37ccafd67cd478cbf4d5
abelanc0 <abelanc0@gmail.com>
2019-08-07 15:34:35 -0400

    Updated ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -58,37 +58,37 @@ resource "aws_instance" "jumpbox" {
 
 }
 
-#resource "aws_security_group" "sec_web" {
-#  name        = "sec_web"
-#  description = "Used for autoscale group"
-#  vpc_id      = "${aws_vpc.default.id}"
-#
-#  # HTTP access from anywhere
+resource "aws_security_group" "sec_jumpbox" {
+  name        = "sec_web"
+  description = "Used for autoscale group"
+  vpc_id      = "${aws_vpc.default.id}"
+
+  # HTTP access from anywhere
 #  ingress {
 #    from_port   = 80
 #    to_port     = 80
 #    protocol    = "tcp"
 #    cidr_blocks = ["0.0.0.0/0"]
 #  }
-#  ingress {
-#    from_port   = 22
-#    to_port     = 22
-#    protocol    = "tcp"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#  
-#  # outbound internet access
-#  egress {
-#    from_port   = 0
-#    to_port     = 0
-#    protocol    = "-1"
-#    cidr_blocks = ["0.0.0.0/0"]
-#  }
-#  lifecycle {
-#    create_before_destroy = true
-#  }
-#}
-#
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 #resource "aws_security_group" "sec_lb" {
 #  name = "sec_elb"
 #  vpc_id      = "${aws_vpc.default.id}"
